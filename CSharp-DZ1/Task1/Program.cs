143db59a5c1ff0e84f63b403bfa83c40173af1e3
Ivan V <wboar@ya.ru>
2022-04-29 10:13:32 +0300

    Added Lesson 2

---
+++ CSharp-DZ1/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double a = 5, b = 7;
+if (a>b)
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}
---
+++ CSharp-DZ1/Task6/Program.cs
@@ -0,0 +1,4 @@
+﻿// Из условия и приведенных примеров, предполагаем что числа на входе могут быть только целыми
+int threeDigitsNumber = 356;
+int lastDigit = threeDigitsNumber % 10;
+Console.WriteLine(lastDigit);
