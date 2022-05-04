a46b1491815e615ad399c76600961ada2c987288
Ivan V <wboar@ya.ru>
2022-05-04 11:21:27 +0300

    Minor changes to Task 5

--- CSharp-DZ4/Lead/Task5/Program.cs
+++ CSharp-DZ4/Lead/Task5/Program.cs
@@ -3,7 +3,7 @@
 // Ряд Фибоначчи - числа, каждое последующее число которого равно сумме двух предыдущих чисел начиная с 0 и 1
 
 
-List<int> BuildFibo(int value)
+/*List<int> BuildFibo(int value)
 {
     List<int> list = new List<int> { };
 
@@ -17,13 +17,38 @@ List<int> BuildFibo(int value)
         list.Add(prev1 + prev2);
     }
     return (list); // тут вернем список
+}*/
+
+string BuildFiboString(int value)
+{
+    string str = String.Empty;
+
+    Int64 prev1 = 0;
+    Int64 prev2 = 1;
+    Int64 summ = 0;
+
+    str = Convert.ToInt64(prev1)+",";
+    str += Convert.ToInt64(prev2);
+
+    for (int i = 2; i < value; i++)
+    {
+        summ = prev1 + prev2;
+        str += ","+Convert.ToInt64(summ);
+        prev1 = prev2;
+        prev2 = summ;
+    }
+    return str;
 }
 
-string PrintListToString(List<int> list)
+/*string PrintListToString(List<int> list)
 {
     return String.Join(',', list);
-}
+}*/
 
 int a = 15;
-List<int> list = BuildFibo(a);
-System.Console.WriteLine(PrintListToString(list));
+string str = String.Empty;
+str = BuildFiboString(a);
+System.Console.WriteLine(str);
+
+//List<int> list = BuildFibo(a);
+//System.Console.WriteLine(PrintListToString(list));
